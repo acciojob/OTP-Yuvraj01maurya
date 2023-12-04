2887a363f25ec214b87859ef1ed47b41f308dab2
Yuvraj Maurya <yuvraj95166@gmail.com>
2023-12-04 21:24:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+let codeInputs = document.querySelectorAll('.code');
+
+function onInput(e) {
+    if (e.target.value) {
+        let nextInput = codeInputs[Array.from(codeInputs).indexOf(e.target) + 1];
+        if (nextInput) {
+            nextInput.focus();
+        }
+    } else {
+        let prevInput = codeInputs[Array.from(codeInputs).indexOf(e.target) - 1];
+        if (prevInput) {
+            prevInput.focus();
+        }
+    }
+}
